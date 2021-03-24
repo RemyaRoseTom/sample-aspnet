d89bcdd2b0cfb499ecb5ab4ce1751fc370994c1c
RemyaRoseTom <remyarosetom@gmail.com>
2021-03-24 13:10:27 +0530

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -4,6 +4,7 @@ namespace sampleweb.Models
 {
     public class ErrorViewModel
     {
+        //requestid 
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
